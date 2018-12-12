ef5cddd45e80d9af3ed678fc9e936192d17d4741
Mihail Sotnichek <sotnichek.mihail@apriorit.com>
2018-12-12 17:27:45 +0200

    Green: added rest of fields to structure

--- tdd_intro/homework/07_sqlite_header_parser/DbHeaderStructure.h
+++ tdd_intro/homework/07_sqlite_header_parser/DbHeaderStructure.h
@@ -23,6 +23,7 @@ const char s_dbDataHeader[]{"SQLite format 3"};
 const unsigned int s_payloadMinFraction = 32;
 const unsigned int s_payloadMaxFraction = 64;
 const unsigned int s_payloadLeafFraction = 32;
+const std::size_t s_reservedEmptySpace = 20;
 
 enum class ReadWriteFormat : uint8_t {
     Undefined = 0,
@@ -32,7 +33,7 @@ enum class ReadWriteFormat : uint8_t {
     Last = ReadWriteFormat::WAL
 };
 
-enum class SchemaFormat : uint16_t {
+enum class SchemaFormat : uint32_t {
     Undefined = 0,
     First = 1,
     FormatOne = SchemaFormat::First,
@@ -42,7 +43,7 @@ enum class SchemaFormat : uint16_t {
     Last = SchemaFormat::FormatFour
 };
 
-enum class Encoding : uint16_t {
+enum class Encoding : uint32_t {
     Undefined = 0,
     First = 1,
     UTF8 = Encoding::First,
@@ -56,11 +57,25 @@ struct DbHeader {
     uint16_t pageSize;
     ReadWriteFormat fileWriteFormatVersion;
     ReadWriteFormat fileReadFormatVersion;
+    uint8_t reservedSpace;
     uint8_t payloadMaxFraction;
     uint8_t payloadMinFraction;
     uint8_t payloadLeafFraction;
+    uint32_t changeCounter;
+    uint32_t sizePages;
+    uint32_t freelistPageTrunkNumber;
+    uint32_t schemaCookie;
     SchemaFormat schemaFormat;
+    uint32_t pageCacheSize;
+    uint32_t bTreeRootPageNumber;
     Encoding encoding;
-};
+    uint32_t userVersion;
+    uint32_t vacuumMode;
+    uint32_t applicationId;
+    char reserved[s_reservedEmptySpace];
+    uint32_t versionValidForNumber;
+    uint32_t sqliteVersionNumber;
+ };
+
 
 void DisplayHeaderStructure(IGui* gui, IDbReader* dbReader);
